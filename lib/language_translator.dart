cd3553d6c6ed754701691e0538996ac951d7033b
Aibek <arstanbekov77@gmail.com>
2026-04-19 13:35:03 +0200

    First commit for translator app

---
+++ lib/language_translator.dart
@@ -0,0 +1,181 @@
+import 'package:flutter/material.dart';
+import 'package:translator/translator.dart';
+
+class LanguageTranlationPage extends StatefulWidget {
+  const LanguageTranlationPage({super.key});
+
+  @override
+  State<LanguageTranlationPage> createState() => _LanguageTranlationPageState();
+}
+
+class _LanguageTranlationPageState extends State<LanguageTranlationPage> {
+  var languages = ['English', 'Kyrgyz', 'German'];
+  var originalLanguage = 'From';
+  var destinationLanguage = 'To';
+  var output = '';
+  TextEditingController languageController = TextEditingController();
+
+  void translate(String src, String dest, String input) async {
+    GoogleTranslator translator = new GoogleTranslator();
+    var translation = await translator.translate(input, from: src, to: dest);
+    setState(() {
+      output = translation.text.toString();
+    });
+    if (src == '--' || dest == '--') {
+      setState(() {
+        output = 'Fail to translate';
+      });
+    }
+  }
+
+  String getLanguageCode(String language) {
+    if (language == 'English') {
+      return 'en';
+    } else if (language == 'Kyrgyz') {
+      return 'ky';
+    } else if (language == 'German') {
+      return 'de';
+    }
+    return '--';
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xff10223d),
+      appBar: AppBar(
+        title: Text(
+          'Language Translator',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 28,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Color(0xff10223d),
+        elevation: 0,
+      ),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(height: 50),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  DropdownButton(
+                    focusColor: Colors.white,
+                    iconDisabledColor: Colors.white,
+                    iconEnabledColor: Colors.white,
+                    hint: Text(
+                      originalLanguage,
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    dropdownColor: Colors.white,
+                    icon: Icon(Icons.keyboard_arrow_down),
+                    items: languages.map((String dropDownStringItem) {
+                      return DropdownMenuItem(
+                        child: Text(dropDownStringItem),
+                        value: dropDownStringItem,
+                      );
+                    }).toList(),
+                    onChanged: (String? value) {
+                      setState(() {
+                        originalLanguage = value!;
+                      });
+                    },
+                  ),
+                  SizedBox(width: 40),
+                  Icon(
+                    Icons.arrow_right_alt_outlined,
+                    color: Colors.white,
+                    size: 40,
+                  ),
+                  SizedBox(width: 40),
+                  DropdownButton(
+                    focusColor: Colors.white,
+                    iconDisabledColor: Colors.white,
+                    iconEnabledColor: Colors.white,
+                    hint: Text(
+                      destinationLanguage,
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    dropdownColor: Colors.white,
+                    icon: Icon(Icons.keyboard_arrow_down),
+                    items: languages.map((String dropDownStringItem) {
+                      return DropdownMenuItem(
+                        child: Text(dropDownStringItem),
+                        value: dropDownStringItem,
+                      );
+                    }).toList(),
+                    onChanged: (String? value) {
+                      setState(() {
+                        destinationLanguage = value!;
+                      });
+                    },
+                  ),
+                ],
+              ),
+              SizedBox(height: 40),
+              Padding(
+                padding: EdgeInsets.all(8),
+                child: TextFormField(
+                  cursorColor: Colors.white,
+                  autofocus: false,
+                  style: TextStyle(color: Colors.white),
+                  decoration: InputDecoration(
+                    labelText: 'Please enter your text...',
+                    labelStyle: TextStyle(fontSize: 15, color: Colors.white),
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white, width: 1),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white, width: 1),
+                    ),
+                    errorStyle: TextStyle(color: Colors.red, fontSize: 15),
+                  ),
+                  controller: languageController,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return ' Please enter text to translate';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(8),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xff2b3c5a),
+                  ),
+                  onPressed: () {
+                    translate(
+                      getLanguageCode(originalLanguage),
+                      getLanguageCode(destinationLanguage),
+                      languageController.text.toString(),
+                    );
+                  },
+                  child: Text(
+                    "Translate",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              Text(
+                "\n$output",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
